cdc62e457bd66792154c8fd63ad43361b0b87704
ManSim <manuelsimeon@yandex.com>
2020-03-08 22:46:30 +0800

    added login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,109 @@
-@extends('layouts.app')
+<!-- W3hubs.com - Download Free Responsive Website Layout Templates designed on HTML5 CSS3,Bootstrap which are 100% Mobile friendly. w3Hubs all Layouts are responsive cross browser supported, best quality world class designs. -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Form Using Materialize Css</title>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <link rel="stylesheet" href="{{asset('assets/css/materializeModified/materialize.min.css')}}">
+<style type="text/css">
+    body{
+        padding: 0;
+        margin: 0;
+        background: #ddd;
+    }
+    .btn{
+        margin-top: 10px;
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    }
+    .container{
+        margin:100px auto;
+        width: 35%;
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+    }
+    @media(max-width: 952px){
+        .container{
+            width: 60%;
+        }
+    }
+        @media(max-width: 475px){
+        .container{
+            width: 80%;
+        }
+    }
+    #neu_logo{
+            height: auto;
+            width: 100%;
+            max-width: 100px;
+            margin-left: 40%;
+            margin-top: -70%;
+            position: absolute;
+            position: relative;
+    display: inline;
+        }
+        
+</style>
+</head>
+<body>
 
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+    <div class="container">
+    <div class="row card hoverable">
+        <div class="card-content ">
+            <img src="{{asset('images/neu_logo.png')}}" id="neu_logo" alt="">
+        <h4 class="center blue-text">Login</h4>
+        <form class="row s12" method="POST"  action="{{ route('login') }}" >
+        @csrf
+        <div class="col s12">
+            <div class="input-field">
+                <input id="email" type="email" placeholder="Email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus autocomplete="off">
+                    @if ($errors->has('email'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
             </div>
         </div>
+        <div class="col s12">
+            <div class="input-field">
+                <input id="password" type="password" placeholder="Password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                    @if ($errors->has('password'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+            </div>
+        </div>
+
+        <!-- forgot password -->
+        
+        <!-- <div class="form-group row mb-0">
+            <div class="col-md-8 offset-md-4">
+                
+
+                @if (Route::has('password.request'))
+                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                        {{ __('Forgot Your Password?') }}
+                    </a>
+                @endif
+            </div>
+        </div> -->
+
+        <!-- end of forgot password -->
+
+        <div class="col s12">
+            <p><label><input type="checkbox"></label></p>
+        </div>
+        <div class="col s12 center">
+            <button type="submit" class="btn btn-large waves-effect waves-light blue">
+                    Login<i class="material-icons right">send</i>
+                </button>
+        </div>
+    </form>
     </div>
 </div>
-@endsection
+</div>
+</body>
+</html>
